aff634691adad253b17d977a254e3de62eaf9f44
pauliddin2 <mr.pauliddin@gmail.com>
2021-08-12 02:00:30 -0700

    small change

--- lib/screens/products_overview_screens.dart
+++ lib/screens/products_overview_screens.dart
@@ -7,6 +7,7 @@ class ProductsOverviewScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [],
         title: Text('MyShop'),
       ),
       body: ProductsGrid(),
