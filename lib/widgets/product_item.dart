779a82f43898cdbf067dba24f4abacd9a252c15d
pauliddin2 <mr.pauliddin@gmail.com>
2021-08-16 14:18:28 +0500

    small change on scaffoldMessanger

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -52,6 +52,7 @@ class ProductItem extends StatelessWidget {
             ),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                 content: Text(
                   'Added item to cart',
